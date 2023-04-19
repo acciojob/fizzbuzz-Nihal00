78ad0005d26415b88898cf13f133a283f214f5f9
Nihal00 <83398133+Nihal00@users.noreply.github.com>
2023-04-19 15:30:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//your JS code here. If required.
+
+let arr = [];
+
+function() {
+	for(let i=0; i<=100; i++){
+		if(i % 3 == 0){
+			arr.push("Fizz");
+		}else if(i % 5 == 0){
+			arr.push("Buzz");
+		}else if(i % 3 == 0 && i % 5 == 0){
+			arr.push("FizzBuzz");
+		}else {
+			arr.push(i);
+		}
+	}
+}
+
